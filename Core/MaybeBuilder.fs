9df8f0b0caa5e4b016253677b3faa16a3d8fa63a
Tim <Tim@TIM-LAPTOP>
2009-05-24 14:52:45 +0000

    Add missing file
    [git-p4: depot-paths = "//depot/Development/Lisp/": change = 685]

---
+++ Core/MaybeBuilder.fs
@@ -0,0 +1,18 @@
+﻿#light
+namespace Tim.Lisp.Core
+
+type MaybeBuilder() =
+    member b.Return(x) = Some x
+    member b.Bind(p, rest) = 
+        match p with
+        | None -> None
+        | Some r -> rest r
+    member b.Delay(f) = f()
+    member b.Let(p, rest) : Option<'a> = rest p
+
+module MaybeBuilderModule =
+    let maybe = MaybeBuilder()
+    let option_of_nullable o =
+        match box o with
+        | null -> None
+        | _ -> Some o
