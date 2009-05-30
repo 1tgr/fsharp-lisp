682b88dac596f091d59d3cb9fbbbe4b289c74250
Tim <Tim@TIM-LAPTOP>
2009-05-30 18:49:01 +0000

    Use LispCodeProvider
    [git-p4: depot-paths = "//depot/Development/Lisp/": change = 691]

--- Compiler/Program.fs
+++ Compiler/Program.fs
@@ -2,22 +2,30 @@
 namespace Tim.Lisp.Compiler
 
 open System
-open System.Reflection
-open System.Reflection.Emit
+open System.CodeDom.Compiler
 open Tim.Lisp.Core
 
 module Program =
     [<EntryPoint>]
     let main (_ : string array) =
         try
-            @"
+            let code = @"
 (define (fact n) (if (= n 0) 1 (* n (fact (- n 1)))))
 (Console.WriteLine ""6! = {0}"" (fact 6))
 (Console.WriteLine ""What is your name?"")
 (Console.WriteLine ""Hello, {0}"" (Console.ReadLine))"
-            |> Parser.parseString 
-            |> Compiler.compileToFile "output.exe" 
-            0
+
+            let provider = new LispCodeProvider()
+            let options = new CompilerParameters()
+            options.OutputAssembly <- "output.exe"
+
+            let results = provider.CompileAssemblyFromSource(options, [| code |])
+            let errors = results.Errors |> Seq.cast<CompilerError>
+            errors |> Seq.iter (fun e -> stderr.WriteLine(e.ErrorText))
+
+            if errors |> Seq.exists (fun e -> not e.IsWarning)
+            then 1
+            else 0
         with ex ->
             stderr.WriteLine(ex)
             1
