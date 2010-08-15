36db2d71769e190eef163acb3e2a49dbcd64b55f
Tim Robinson <tim.g.robinson@gmail.com>
2010-08-15 18:28:45 +0100

    Start emitting functions

--- Core/compiler.fs
+++ Core/compiler.fs
@@ -1,97 +1,137 @@
 ﻿#light
 namespace Tim.Lisp.Core
 open System
+open System.IO
+open System.Reflection
+open System.Reflection.Emit
 open Tim.Lisp.Core.Syntax
 
 module internal CompilerImpl =
-    type InitEnv =
+    type Block<'a> =
         {
-            Parent : InitEnv option
-            Values : Map<string, Expr<unit>>
+            Body : Stmt<'a> list
         }
 
-    type Stmt<'a, 'exprTag> = Scope of 'a * Stmt<'a, 'exprTag> list
-                            | Stmt of 'a * Expr<'exprTag>
+    and Stmt<'a> = Block of Env<'a> * Block<'a>
+                 | Expr of Env<'a> * Expr<'a>
 
-    let makeScope (env : InitEnv) (exprs : Expr<'a> list) : Stmt<InitEnv, 'a> =
+    and Func<'a> =
+        {
+            Block : Block<'a>
+            Params : string list
+        }
+
+    and EnvValue<'a> = Func of Func<'a>
+                     | Var of Expr<'a>
+
+    and Env<'a> =
+        {
+            Parent : Env<'a> option
+            Func : Func<'a> ref
+            Values : Map<string, EnvValue<'a>>
+        }
+
+    let makeBlock (env : Env<'a>) (exprs : Expr<'a> list) : Env<'a> * Block<'a> =
         let rec addToScope
-            (env   : InitEnv, body : Stmt<InitEnv, 'a> list) 
+            (env   : Env<'a>, block : Block<'a>) 
             (exprs : Expr<'a> list) 
-                   : InitEnv * Stmt<InitEnv, 'a> list
+                   : Env<'a> * Block<'a>
             =
             match exprs with
             | List(_, Atom(_, "define") :: values) :: tail ->
-                match values with
-                | [Atom(_, name); value] ->
-                    let value = value |> Expr.mapa (fun _ -> ())
+                let name, value = 
+                    match values with
+                    | [Atom(_, name); value] -> name, Var value
+                    | List _ :: _ -> failwithf "Functions not yet implemented"
+                    | _ -> failwithf "define expected 1 value, not %A" values
 
-                    match body with
-                    | [] ->
-                        tail |> addToScope ({ env with Values = Map.add name value env.Values }, [])
-
-                    | _ ->
-                        let ienv = { Parent = Some env
-                                     Values = Map.ofList [(name, value)] }
-
-                        let ienv, ibody = tail |> addToScope (ienv, [])
-                        env, body @ [Scope(ienv, ibody)]
-
-                | List _ :: _ ->
-                    failwithf "Functions not yet implemented"
+                match block with
+                | { Body = [] } ->
+                    tail |> addToScope ({ env with Values = Map.add name value env.Values }, { Body = [] })
 
                 | _ ->
-                    failwithf "define expected 1 value, not %A" values
+                    let ienv = { env with Parent = Some env
+                                          Values = Map.ofList [(name, value)] }
+
+                    let ienv, iblock = tail |> addToScope (ienv, { Body = [] })
+                    env, { block with Body = block.Body @ [Block(ienv, iblock)] }
 
             | head :: tail ->
-                let scope = env, body @ [Stmt(env, head)]
+                let scope = env, { block with Body = block.Body @ [Expr(env, head)] }
                 tail |> addToScope scope
 
             | [] ->
-                env, body
+                env, block
 
-        exprs
-        |> addToScope (env, List.empty)
-        |> Scope
+        addToScope (env, { Body = [] }) exprs
 
-    let rec resolveLocal (env : InitEnv) (expr : Expr<'a>) : Expr<'a> =
-        match expr with
-        | Atom(a, name) ->            
-            match Map.tryFind name env.Values with
-            | Some initExpr -> Expr.mapa (fun () -> a) initExpr
-            | None ->
-                match env.Parent with
-                | Some env -> resolveLocal env expr
-                | None -> failwithf "%s: undeclared identifier" name
+    type ILFunction(typeBuilder : TypeBuilder, name : string, paramNames : string list) =
+        let dynamicMethod = 
+            typeBuilder.DefineMethod(
+                name, 
+                MethodAttributes.Public ||| MethodAttributes.Static, 
+                typeof<Void>, 
+                paramNames |> List.map (fun _ -> typeof<int>) |> Array.ofList)
 
-        | List(a, exprs) ->
-            let exprs = exprs |> List.map (resolveLocal env)
-            List(a, exprs)
+        let generator = dynamicMethod.GetILGenerator()
 
-        | expr ->
-            expr
+        member this.DynamicMethod = dynamicMethod
+        member this.Generator = generator
 
-    let rec resolveLocals (stmt : Stmt<InitEnv, 'a>) : Stmt<unit, 'a> =
+    let rec makeILFunctionsFromValue (typeBuilder : TypeBuilder) (name : string) (value : EnvValue<'a>) : (ILFunction * Func<'a>) list =
+        match value with
+        | Func func ->
+            let ilFunc = ILFunction(typeBuilder, name, func.Params)
+            (ilFunc, func) :: List.collect (makeILFunctions typeBuilder) func.Block.Body
+
+        | Var _ ->
+            []
+
+    and makeILFunctions (typeBuilder : TypeBuilder) (stmt : Stmt<'a>) : (ILFunction * Func<'a>) list =
         match stmt with
-        | Scope(_, body) ->
-            let body = body |> List.map resolveLocals
-            Scope((), body)
+        | Block(env, block) ->
+            env.Values
+            |> Map.toList
+            |> List.collect (fun (a, b) -> makeILFunctionsFromValue typeBuilder a b)
+            |> List.append (List.collect (makeILFunctions typeBuilder) block.Body)
 
-        | Stmt(env, expr) ->
-            Stmt((), resolveLocal env expr)
+        | Expr _ ->
+            []
 
-    let rec print (indent : int) (stmt : Stmt<_, _>) : string list =
-        let prefix = new string(' ', indent)
-        match stmt with
-        | Scope(_, body) -> (sprintf "%sScope:" prefix) :: (body |> List.collect (print (indent + 1)))
-        | Stmt(_, expr) -> [expr |> sprintf "%s%A" prefix]
+    let emitFunc (ilFunc : ILFunction) (func : Func<_>) : unit =
+        let emitExpr (expr : Expr<_>) : unit =
+            ()
+
+        let rec emitStmt (stmt : Stmt<_>) : unit =
+            match stmt with
+            | Block(_, block) ->
+                List.iter emitStmt block.Body
+
+            | Expr(_, expr) ->
+                emitExpr expr
+
+        List.iter emitStmt func.Block.Body
+        ilFunc.Generator.Emit(OpCodes.Ret)
+
+open CompilerImpl
 
 module Compiler =
-    let compileToDelegate (delegateType : Type) (code : Expr<_> list) : Delegate =
-        let msg = 
-            code
-            |> CompilerImpl.makeScope { Parent = None; Values = Map.empty }
-            |> CompilerImpl.resolveLocals
-            |> CompilerImpl.print 0
-            |> String.concat "\n"
+    let compileToDelegate (delegateType : Type) (code : Expr<'a> list) : Delegate =
+        let main =
+            let mainRef = ref { Block = { Body = [] }; Params = [] }
+            let mainEnv, mainBlock = makeBlock { Parent = None; Func = mainRef; Values = Map.empty } code
+            mainRef := { !mainRef with Block = mainBlock }
+            Func !mainRef
 
-        raise <| NotImplementedException ("\n" + msg)
+        let name = AssemblyName("DynamicAssembly")
+        let assemblyBuilder = AppDomain.CurrentDomain.DefineDynamicAssembly(name, AssemblyBuilderAccess.RunAndSave)
+        let moduleBuilder = assemblyBuilder.DefineDynamicModule(name.Name + ".dll")
+        let typeBuilder = moduleBuilder.DefineType("Program")
+        
+        for (ilFunc, func) in makeILFunctionsFromValue typeBuilder "main" main do
+            emitFunc ilFunc func
+
+        let t = typeBuilder.CreateType()
+        assemblyBuilder.Save(name.Name + ".dll")
+        let methodInfo = t.GetMethod("main", BindingFlags.Public ||| BindingFlags.Static)
+        Delegate.CreateDelegate(delegateType, methodInfo)
