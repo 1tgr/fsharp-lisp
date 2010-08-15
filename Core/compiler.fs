dad21c4f7d3ae12c295863237c2812b958adaca3
Tim Robinson <tim.g.robinson@gmail.com>
2010-08-15 15:48:32 +0100

    Don't need LocalEnv yet

--- Core/compiler.fs
+++ Core/compiler.fs
@@ -47,41 +47,13 @@ module internal CompilerImpl =
         |> addToScope (Map.empty, List.empty)
         |> Scope
 
-    type Local =
-        {
-            Name : string
-            InitExpr : Expr<unit>
-        }
-
-    type LocalEnv = Map<string, Local ref>
-
-    let addToLocals (locals : LocalEnv) (name : string) (initExpr : Expr<unit>) : LocalEnv =
-        match Map.tryFind name locals with
-        | Some ref when (!ref).InitExpr = initExpr ->
-            locals
-
-        | _ ->
-            let local = { Name = name
-                          InitExpr = initExpr }
-            Map.add name (ref local) locals
-
-    let rec allocateLocals (locals : LocalEnv) (stmt : Stmt<InitEnv, 'a>) : Stmt<LocalEnv, 'a> =
-        match stmt with
-        | Scope(initEnv, body) ->
-            let localEnv = initEnv |> Map.fold addToLocals locals
-            let body = body |> List.map (allocateLocals localEnv)
-            Scope(localEnv, body)
-
-        | Stmt(_, expr) ->
-            Stmt(locals, expr)
-
-    let rec resolveLocal (parentEnvs : LocalEnv list) (expr : Expr<'a>) : Expr<'a> =
+    let rec resolveLocal (parentEnvs : InitEnv list) (expr : Expr<'a>) : Expr<'a> =
         match expr with
         | Atom(a, name) ->
             match parentEnvs with
             | head :: tail ->
                 match Map.tryFind name head with
-                | Some ref -> Expr.mapa (fun () -> a) ((!ref).InitExpr)
+                | Some initExpr -> Expr.mapa (fun () -> a) initExpr
                 | None -> resolveLocal tail expr
 
             | [] ->
@@ -94,7 +66,7 @@ module internal CompilerImpl =
         | expr ->
             expr
 
-    let rec resolveLocals (parentEnvs : LocalEnv list) (stmt : Stmt<LocalEnv, 'a>) : Stmt<unit, 'a> =
+    let rec resolveLocals (parentEnvs : InitEnv list) (stmt : Stmt<InitEnv, 'a>) : Stmt<unit, 'a> =
         match stmt with
         | Scope(localEnv, body) ->
             let body = body |> List.map (resolveLocals (localEnv :: parentEnvs))
@@ -114,7 +86,6 @@ module Compiler =
         let msg = 
             code
             |> CompilerImpl.makeScope
-            |> CompilerImpl.allocateLocals Map.empty
             |> CompilerImpl.resolveLocals List.empty
             |> CompilerImpl.print 0
             |> String.concat "\n"
